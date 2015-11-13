19fee31ed83f9e02ca1dad5298f0385478a9a6c3
zodern <zodern@users.noreply.github.com>
2015-11-12 19:19:45 -0600

    Remove console.logs

--- publish.js
+++ publish.js
@@ -1,14 +1,11 @@
 Meteor.publish('_pa.Mongo.collection', function (collectionName) {
-  console.log('name', collectionName);
   if(PureAdmin.isAdmin(this.userId) === false) {
     return [];
   }
-  //console.log(Mongo.Collection.get(collectionName).find().fetch());
   return Mongo.Collection.get(collectionName).find();
 });
 
 Meteor.publish('_pa.Mongo.document', function (info) {
-  console.log(info);
   if(PureAdmin.isAdmin(this.userId) === false) {
     return [];
   }
